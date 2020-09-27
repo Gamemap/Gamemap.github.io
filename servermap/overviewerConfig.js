c00d3a8fd4fc78ed466002a76f3d87c51ca1129d
Gamemap <71942164+Gamemap@users.noreply.github.com>
2020-09-27 09:02:07 +0200

    Updated

---
+++ servermap/overviewerConfig.js
@@ -0,0 +1,200 @@
+var overviewerConfig = {
+    "CONST": {
+        "tileSize": 384,
+        "image": {
+            "defaultMarker": "signpost.png",
+            "signMarker": "signpost_icon.png",
+            "bedMarker": "bed.png",
+            "spawnMarker": "markers/marker_home.png",
+            "spawnMarker2x": "markers/marker_home_2x.png",
+            "queryMarker": "markers/marker_location.png",
+            "queryMarker2x": "markers/marker_location_2x.png",
+            "compass": {
+                "0": "compass_upper-left.png",
+                "1": "compass_upper-right.png",
+                "3": "compass_lower-left.png",
+                "2": "compass_lower-right.png"
+            }
+        },
+        "mapDivId": "mcmap",
+        "UPPERLEFT": 0,
+        "UPPERRIGHT": 1,
+        "LOWERLEFT": 3,
+        "LOWERRIGHT": 2
+    },
+    "worlds": [
+        "world - overworld",
+        "world_the_end - end",
+        "world_nether - nether"
+    ],
+    "map": {
+        "debug": false,
+        "cacheTag": "1600200639",
+        "north_direction": "lower-left",
+        "controls": {
+            "pan": true,
+            "zoom": true,
+            "spawn": true,
+            "compass": true,
+            "mapType": true,
+            "overlays": true,
+            "coordsBox": true
+        }
+    },
+    "tilesets": [
+        {
+            "name": "C4 Villa Norden",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "worlddaynorth",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world - overworld",
+            "last_rendertime": 1600011495,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -64,
+                96,
+                -143
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -64,
+                96,
+                -143
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "C4 Villa S\u00fcden",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "worlddaysouth",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world - overworld",
+            "last_rendertime": 1600011495,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -64,
+                96,
+                -143
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -64,
+                96,
+                -143
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "C4 Villa Nacht Norden",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "worldnightnorth",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world - overworld",
+            "last_rendertime": 1600011495,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -64,
+                96,
+                -143
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -64,
+                96,
+                -143
+            ],
+            "north_direction": 0
+        },
+        {
+            "name": "C4 Villa Nacht S\u00fcden",
+            "zoomLevels": 11,
+            "defaultZoom": 1,
+            "maxZoom": 11,
+            "path": "worldnightsouth",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world - overworld",
+            "last_rendertime": 1600011495,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                -64,
+                96,
+                -143
+            ],
+            "minZoom": 0,
+            "spawn": [
+                -64,
+                96,
+                -143
+            ],
+            "north_direction": 2
+        },
+        {
+            "name": "C4 Villa End",
+            "zoomLevels": 6,
+            "defaultZoom": 1,
+            "maxZoom": 6,
+            "path": "worldend",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world_the_end - end",
+            "last_rendertime": 1600011321,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                64,
+                0
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        },
+        {
+            "name": "C4Villa Nether",
+            "zoomLevels": 10,
+            "defaultZoom": 1,
+            "maxZoom": 10,
+            "path": "worldnether",
+            "base": "",
+            "bgcolor": "#1a1a1a",
+            "world": "world_nether - nether",
+            "last_rendertime": 1600011321,
+            "imgextension": "png",
+            "isOverlay": false,
+            "poititle": "Markers",
+            "showlocationmarker": true,
+            "center": [
+                0,
+                64,
+                0
+            ],
+            "minZoom": 0,
+            "spawn": false,
+            "north_direction": 0
+        }
+    ]
+};
